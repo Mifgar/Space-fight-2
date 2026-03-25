434c4324c5f98e40d5fffdde14bc181bb1552882
Mifgar <162031063+Mifgar@users.noreply.github.com>
2026-03-25 06:35:19 +0100

    fix: favicon, mobile-Layout behind settingsMenu, Eventlog mute and multiple executions

--- EventLog.js
+++ EventLog.js
@@ -1,21 +1,32 @@
 let EventBoxTimeout = null;
+let typingActive = false;
 
 function typewriterLog(message, speed = 30, hideDelay = 5000) {
     const logBox = document.getElementById("eventLog");
     if (!logBox) return;
 
+    typingActive = false;
+
     logBox.textContent = '';
     clearTimeout(EventBoxTimeout);
     logBox.classList.remove("hidden");
+
+    const token = Symbol();
+    typingActive = token;
+
     let i = 0;
 
     function type() {
+
+        if (typingActive !== token) return;
+
         if (i < message.length) {
             const char = message.charAt(i);
             logBox.textContent += char;
 
             const typeVolume = parseFloat(TypeSoundSlider.value);
             const sound = typeSound[Math.floor(Math.random() * typeSound.length)].cloneNode();
+            sound.muted = !togglesound;
             sound.volume = Math.min(typeVolume * (Math.random() * 0.4 + 0.6), 1.0);
             sound.play().catch(() => {});
 
